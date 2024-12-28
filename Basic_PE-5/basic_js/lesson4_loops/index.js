6bc07560e73861d3f09009c6fd2504cf45ec4975
Stan <stanjke@gmail.com>
2024-12-28 14:00:46 +0200

    renew

---
+++ Basic_PE-5/basic_js/lesson4_loops/index.js
@@ -0,0 +1,27 @@
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+
+let limit = 200;
+let start = 0;
+
+//       6  <=   5  true
+while (start <= limit) {
+  console.log("Hello world!" + start);
+  start++;
+}
+// 1st iteration
+// iteration start = 0
+// start <= limit  - true
+// 2nd iteration
+//  iteration start = 1
+//  start <= limit - true
+// .....
+//
