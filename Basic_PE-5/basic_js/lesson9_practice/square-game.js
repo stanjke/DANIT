10d27add4f97ed01b52a32ed05ad62f47ba575ad
Stanislav <s.ostapushchenko@omisoft.com>
2025-01-25 14:40:27 +0200

    add lesson 9 practice fn

--- Basic_PE-5/basic_js/lesson9_practice/square-game.js
+++ Basic_PE-5/basic_js/lesson9_practice/square-game.js
@@ -5,4 +5,16 @@ square.style.height = "115px";
 square.style.width = "115px";
 square.style.backgroundColor = blue;
 
+let x = 0;
+let y = 0;
+let dirX = 1;
+let dirY = 1;
+
+function moveSquare() {
+  x += dirX;
+  y += dirY;
+  square.style.top = y + "px";
+  square.style.left = x + "px";
+}
+
 document.body.appendChild(square);
